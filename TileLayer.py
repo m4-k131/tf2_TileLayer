5dc9f7d0b35644d0d26e4443c746c2ac3e467ad6
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2021-03-10 13:03:35 +0100

    Add files via upload

---
+++ TileLayer.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Malte Kleine
+"""
+from tensorflow import reshape, transpose
+from tensorflow.keras import layers
+
+class Tile2D(layers.Layer):
+    def __init__(self, n):
+        super(Tile2D, self).__init__()
+        self.n=n
+    def build(self, input_shape):
+        self.tw = input_shape[1]//self.n
+        self.th = input_shape[2]//self.n
+        self.channels=input_shape[3]
+ 
+    def call(self, images):
+        tiles = reshape(images, (-1 ,self.n, self.tw, self.n, self.th, self.channels))
+        tiles = transpose(tiles, [0, 1, 3, 2, 4, 5])
+        tiles = reshape(tiles, (-1, self.n*self.n, self.tw, self.th, self.channels))
+        return tiles
+
+class Untile2D(layers.Layer):
+    def __init__(self, n):
+        super(Untile2D, self).__init__()
+        self.n=n                   
+    def build(self, input_shape):
+        self.tw=input_shape[2]
+        self.th=input_shape[3]
+        self.iw = self.tw*self.n
+        self.ih = self.th*self.n
+        self.channels=input_shape[4]
+                           
+    def call(self, tiles):
+ 
+        images= reshape(tiles, (-1, self.n, self.n, self.pw, self.ph, self.channels))
+        images = transpose(images, [0, 1, 3, 2, 4, 5])
+        images= reshape(images, (-1, self.iw, self.ih, self.channels))
+        return images
