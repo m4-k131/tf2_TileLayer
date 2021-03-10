2b9dba9b69fc9a51446d13e3cb6ba9f8df6b7162
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2021-03-10 13:22:51 +0100

    Update TileLayer.py

--- TileLayer.py
+++ TileLayer.py
@@ -8,11 +8,12 @@ from tensorflow.keras import layers
 class Tile2D(layers.Layer):
     def __init__(self, n):
         super(Tile2D, self).__init__()
-        self.n=n
+        self.n = n
+        
     def build(self, input_shape):
         self.tw = input_shape[1]//self.n
         self.th = input_shape[2]//self.n
-        self.channels=input_shape[3]
+        self.channels = input_shape[3]
  
     def call(self, images):
         tiles = reshape(images, (-1 ,self.n, self.tw, self.n, self.th, self.channels))
@@ -23,17 +24,17 @@ class Tile2D(layers.Layer):
 class Untile2D(layers.Layer):
     def __init__(self, n):
         super(Untile2D, self).__init__()
-        self.n=n                   
+        self.n = n
+        
     def build(self, input_shape):
-        self.tw=input_shape[2]
-        self.th=input_shape[3]
+        self.tw = input_shape[2]
+        self.th = input_shape[3]
         self.iw = self.tw*self.n
         self.ih = self.th*self.n
-        self.channels=input_shape[4]
+        self.channels = input_shape[4]
                            
     def call(self, tiles):
- 
-        images= reshape(tiles, (-1, self.n, self.n, self.pw, self.ph, self.channels))
+        images = reshape(tiles, (-1, self.n, self.n, self.pw, self.ph, self.channels))
         images = transpose(images, [0, 1, 3, 2, 4, 5])
-        images= reshape(images, (-1, self.iw, self.ih, self.channels))
+        images = reshape(images, (-1, self.iw, self.ih, self.channels))
         return images
