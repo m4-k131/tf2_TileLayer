6cdc37a3f642f2072e7649260a4ca21770058ed3
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2021-03-10 13:23:49 +0100

    Update TileLayer.py

--- TileLayer.py
+++ TileLayer.py
@@ -14,11 +14,12 @@ class Tile2D(layers.Layer):
         self.tw = input_shape[1]//self.n
         self.th = input_shape[2]//self.n
         self.channels = input_shape[3]
+        self.nsqrd=self.n**2
  
     def call(self, images):
         tiles = reshape(images, (-1 ,self.n, self.tw, self.n, self.th, self.channels))
         tiles = transpose(tiles, [0, 1, 3, 2, 4, 5])
-        tiles = reshape(tiles, (-1, self.n*self.n, self.tw, self.th, self.channels))
+        tiles = reshape(tiles, (-1, self.nsqrd, self.tw, self.th, self.channels))
         return tiles
 
 class Untile2D(layers.Layer):
